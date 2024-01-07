05adf82907f6a43d17253ed5002049d952e2788f
khosrora <khosrora153333@gmail.com>
2024-01-07 19:39:34 +0330

    connect to elastic search

---
+++ server/2-notification-service/src/elasticsearch.ts
@@ -0,0 +1,25 @@
+import { Client } from '@elastic/elasticsearch';
+import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types';
+import { winstonLogger } from '@khosrora/jobber-shared';
+import { Logger } from 'winston';
+import { config } from './config';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'elastic search server', 'debug');
+
+const elasticSearchClient = new Client({
+  node: `${config.ELASTIC_SEARCH_URL}`
+});
+
+export async function checkConnection(): Promise<void> {
+  let isConnected = false;
+  while (!isConnected) {
+    try {
+      const health: ClusterHealthResponse = await elasticSearchClient.cluster.health({});
+      log.info(`notification service elastic search health status - ${health.status}`);
+      isConnected = true;
+    } catch (error) {
+      log.error('connection to Elastic search faild .. retrying ...');
+      log.log('error', 'notification service checkConnection() : ', error);
+    }
+  }
+}
--- server/2-notification-service/src/server.ts
+++ server/2-notification-service/src/server.ts
@@ -1,35 +1,40 @@
 import 'express-async-errors';
-import http from 'http'
-import { Logger } from 'winston'
-import { winstonLogger } from '@khosrora/jobber-shared'
-import { config } from '@notifications/config';
+import http from 'http';
 import { Application } from 'express';
+
+import { Logger } from 'winston';
+import { winstonLogger } from '@khosrora/jobber-shared';
+
+import { config } from '@notifications/config';
+import { healthRoutes } from '@notifications/routes';
+import { checkConnection } from '@notifications/elasticsearch';
 
 const SERVER_PORT = 4001;
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'notification Server', 'debug');
 
 export function start(app: Application): void {
-    startServer(app);
-    startQueues()
-    startElasticSearch();
+  startServer(app);
+
+  // http:localhost:4001/notificaton-health
+  app.use('', healthRoutes);
+  startQueues();
+  startElasticSearch();
 }
 
-async function startQueues(): Promise<void> {
-
-}
+async function startQueues(): Promise<void> {}
 
 function startElasticSearch(): void {
-
+  checkConnection();
 }
 
 function startServer(app: Application): void {
-    try {
-        const httpServer: http.Server = new http.Server(app);
-        log.info(`worker with process id of ${process.pid} on notification server has started`)
-        httpServer.listen(SERVER_PORT, () => {
-            log.info(`notification server running on port ${SERVER_PORT}`)
-        })
-    } catch (error) {
-        log.log('error', 'NOTIFICATIONSERVICE Start server() method', error);
-    }
+  try {
+    const httpServer: http.Server = new http.Server(app);
+    log.info(`worker with process id of ${process.pid} on notification server has started`);
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`notification server running on port ${SERVER_PORT}`);
+    });
+  } catch (error) {
+    log.log('error', 'NOTIFICATIONSERVICE Start server() method', error);
+  }
 }
